8f2d609e9963b24b43853560d7974b0b43016358
Ge Yang <yangge1987@gmail.com>
2021-01-21 19:54:38 -0500

    update the ssh example config

--- 03_multiple_ssh_reacheable_machines/multi_launch.py
+++ 03_multiple_ssh_reacheable_machines/multi_launch.py
@@ -4,7 +4,7 @@ from launch_entry import train_fn
 
 if __name__ == "__main__":
     for i in range(3):
-        jaynes.config(verbose=False, runner=dict(host=f"vision0{i}"))
+        jaynes.config(verbose=False, runner=dict(host=f"visiongpu0{i}"))
         jaynes.run(train_fn)
 
     jaynes.listen(200)
