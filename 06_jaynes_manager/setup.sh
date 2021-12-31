b915a4ca28f91cc8fdac18a47d38742d6b7d14b7
Ge Yang <geyang@mit.edu>
2021-12-31 12:40:32 -0500

    Update setup.sh

--- 06_jaynes_manager/setup.sh
+++ 06_jaynes_manager/setup.sh
@@ -1,5 +1,23 @@
 module load cuda/10.2
 module load anaconda/2021b
 
+echo 'export DATA_DIR=$HOME/escher_shared' >> $HOME/.bashrc
+
+echo '
+export ML_LOGGER_ROOT=http://44.241.150.228:8080
+export ML_LOGGER_USER=geyang
+export ML_LOGGER_TOKEN=' >> $HOME/.bashrc
+
+echo "
+# manually add the local path
+export PATH=$HOME/.local/bin:$PATH
+
+export PROXY_PORT=$RANDOM
+export http_proxy=http://\$HOSTNAME:\$PROXY_PORT
+export https_proxy=http://\$HOSTNAME:\$PROXY_PORT
+# This line runs the proxy in a screen session in the background
+screen -dm proxy --hostname 0.0.0.0 --port $PROXY_PORT --timeout 3600 --client-recvbuf-size 131072 --server-recvbuf-size 131072
+" >> haha.sh
+
 conda install --yes pyCurl
 pip install -r requirements.txt
