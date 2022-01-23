b9fa3e23d1563a579ce6fb40620c9bc5212d8c82
Ge Yang <yangge1987@gmail.com>
2022-01-23 01:18:15 -0500

    remove secret file

--- 12_FAS_cannon_cluster_setup/launch_entry.py
+++ 12_FAS_cannon_cluster_setup/launch_entry.py
@@ -9,8 +9,8 @@ def launch(pause=1):
 
 
 if __name__ == "__main__":
-    jaynes.config(launch=dict(timeout=2))
-    for i in range(5):
+    jaynes.config(launch=dict(timeout=5), runner=dict(partition="gpu_test"))
+    for i in range(10):
         stdout, stderr, is_err = jaynes.run(launch, pause=100)
-        print(stdout)
+        print(stdout, stderr, is_err)
     jaynes.listen()
