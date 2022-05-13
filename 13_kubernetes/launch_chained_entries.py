f348456ae889768098813d27f70c32e4502bb26d
Ge Yang <yangge1987@gmail.com>
2022-05-12 21:04:10 -0400

    bump version

--- 13_kubernetes/launch_chained_entries.py
+++ 13_kubernetes/launch_chained_entries.py
@@ -6,7 +6,7 @@ def train_fn(seed=None):
 
     sleep(1)
     if seed:
-        print(f"{logger.slurm_job_id} seed={seed}")
+        print(f"{logger.hostname} seed={seed}")
     else:
         print('done.')
 
@@ -15,15 +15,6 @@ if __name__ == "__main__":
     import jaynes
 
     jaynes.config()
-    jaynes.add(train_fn, seed=100) \
-        .chain(train_fn, seed=200) \
-        .chain(train_fn, seed=300) \
-        .chain(train_fn, seed=400) \
-        .chain(train_fn, seed=500)
-    jaynes.add(train_fn, seed=600) \
-        .chain(train_fn, seed=700) \
-        .chain(train_fn, seed=800) \
-        .chain(train_fn, seed=900) \
-        .chain(train_fn, seed=1000)
+    jaynes.add(train_fn, seed=100).chain(train_fn, seed=200).chain(train_fn, seed=300).chain(train_fn, seed=400)
     jaynes.execute()
     jaynes.listen()
