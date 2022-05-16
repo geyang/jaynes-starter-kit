b84ad2f01a280c8cd9787bd02678d67862403629
Ge Yang <yangge1987@gmail.com>
2022-05-16 15:11:08 -0400

    add tailing command

--- 13_kubernetes/launch_entry.py
+++ 13_kubernetes/launch_entry.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     import jaynes
 
     jaynes.config(verbose=True)
-    jaynes.run(train_fn, seed=100)
-    jaynes.listen()
+    job_ids = jaynes.run(train_fn, seed=100)
+    jaynes.listen(command=f"kubectl logs -f {job_ids[0]} --all-containers", interval=3)
