5fc3a9626e901c52b274ea85227fe4a471c92c39
Ge Yang <yangge1987@gmail.com>
2022-05-18 18:21:18 -0400

    add kube example

--- 13_kubernetes/launch_chained_entries.py
+++ 13_kubernetes/launch_chained_entries.py
@@ -15,9 +15,7 @@ if __name__ == "__main__":
     import jaynes
 
     jaynes.config()
-    jaynes.add(train_fn, seed=100).chain(train_fn, seed=200).chain(train_fn, seed=300).chain(train_fn, seed=400)
-    jaynes.add(train_fn, seed=100).chain(train_fn, seed=200).chain(train_fn, seed=300).chain(train_fn, seed=400)
+    jaynes.add(train_fn, seed=100).chain(train_fn, seed=200).chain(train_fn, seed=300)
+    jaynes.add(train_fn, seed=400).chain(train_fn, seed=500).chain(train_fn, seed=600)
     job_ids = jaynes.execute()
     jaynes.listen(command=f"/bin/bash -c 'kubectl logs {job_ids[0]} --follow --all-containers'", backoff_limit=3)
-    jaynes.listen(command=f"/bin/bash -c 'kubectl logs {job_ids[1]} --follow --all-containers'", backoff_limit=3)
-    # jaynes.listen(interval=1)
