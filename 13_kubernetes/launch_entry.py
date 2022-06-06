844f23d6920ca81854c254055b06d681b28e0960
Ge Yang <yangge1987@gmail.com>
2022-06-06 00:25:24 -0400

    add GPU type selection

--- 13_kubernetes/launch_entry.py
+++ 13_kubernetes/launch_entry.py
@@ -7,6 +7,6 @@ def train_fn(seed=None):
 if __name__ == '__main__':
     import jaynes
 
-    jaynes.config(verbose=True)
+    jaynes.config(verbose=False)
     job_ids = jaynes.run(train_fn, seed=100)
     jaynes.listen(command=f"kubectl logs -f {job_ids[0]} --all-containers", interval=3)
